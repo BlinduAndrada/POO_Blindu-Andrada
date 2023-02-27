e48acc257eb1f59644984ed45f4341347ce08557
Andrada Blindu <114437802+BlinduAndrada@users.noreply.github.com>
2023-02-28 00:51:52 +0200

    Update main.cpp

--- seminar2/Problema_1/main.cpp
+++ seminar2/Problema_1/main.cpp
@@ -2,18 +2,31 @@
 
 int main(){
 
-	int vec[10] = { 3, 9, -1, 2, 10, 1, -5, 6, 7, 12 };
-	int count = 5; 
-	NumberList obj(vec, count);
+	//int vec[10] = { 3, 9, -1, 2, 10, 1, -5, 6, 7, 12 };
+	//int count = 5; 
+//	NumberList obj(vec, count);
+	NumberList list;
 
-	obj.Init();
-	obj.Print();
+	//obj.Init();
+	//obj.Print();
 
-	obj.Add(3);
-	obj.Print();
+	//obj.Add(3);
+	//obj.Print();
 
-	obj.Sort();
-	obj.Print();
+	//obj.Sort();
+	//obj.Print();
 
+
+
+	list.Init();
+	list.Add(3);
+	list.Add(1);
+	list.Add(4);
+	list.Add(2);
+	list.Add(7);
+	list.Add(9);
+	list.Print();
+	list.Sort();
+	list.Print();
 	return 0;
 }
